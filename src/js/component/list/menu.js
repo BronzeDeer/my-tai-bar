51fdb68d69f3fc6a25470b5e2abd16081f5d04a2
Joel Pepper <pepper@bronze-deer.de>
2022-08-12 17:18:13 +0200

    menu: Allow passing price to the list and elements

--- src/js/component/list/menu.js
+++ src/js/component/list/menu.js
@@ -3,12 +3,13 @@ import CocktailList from "/js/component/list/base.js"
 
 export default class MenuList extends CocktailList{
 
-    makeLoaderFunc = (id) => {
+    makeLoaderFunc = (id,price) => {
         return  async () =>{
             let el = document.createElement("menu-item")
             el.cocktailID = id
             el.className = "fade-in"
             el.printable = this.printable
+            if (price) el.price = price
             return el
         }
     }
