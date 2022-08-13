af2d4f1fc5649b3867e7169b57947d457db869b9
Joel Pepper <pepper@bronze-deer.de>
2022-08-13 23:42:08 +0200

    components: tag-icons: create rudimentary icon legen

--- src/js/component/tag-icons.js
+++ src/js/component/tag-icons.js
@@ -3,13 +3,35 @@ import BaseComponent from "./base.js"
 let icon_root = "/img/tag-icons/"
 
 let iconized_tags = {
-  "milk": icon_root+"cow.svg",
-  "strong": icon_root+"strong_inverted.svg",
-  "non-alcoholic": icon_root+"non-alc.svg",
-  "grapefruit": icon_root+"grapefruit.svg",
-  "vegan": icon_root+"vegan.svg",
+  "milk": {
+    img_url: icon_root+"cow.svg",
+    desc: "contains milk"
+  },
+  "strong": {
+    img_url: icon_root+"strong_inverted.svg",
+    desc: "very high alcohol content even if it doesn't taste like it; drink slowly"
+  },
+  "non-alcoholic": {
+    img_url: icon_root+"non-alc.svg",
+    desc: "non-alcoholic drink"
+  },
+  "grapefruit": {
+    img_url: icon_root+"grapefruit.svg",
+    desc: "contains grapefruit, may interfere with medication; check medicine leaflet"
+  },
+  "vegan": {
+    img_url: icon_root+"vegan.svg",
+    desc: "contains only vegan ingredients; filtration methods not always known"
+  }
 }
 
+let constructIcon = (item) =>{
+  let el = document.createElement("img")
+  el.style = "height: 3rem; width: 3rem;"
+  el.className = "mx-2"
+  el.src = item.img_url
+  return el
+}
 
 export default class TagIcons extends BaseComponent{
   constructor() {
@@ -45,11 +67,7 @@ export default class TagIcons extends BaseComponent{
                 for (let tag of newValue.split(",").map((x)=>x.trim())){
 
                   if( tag in iconized_tags){
-                    let el = document.createElement("img")
-                    el.style = "height: 3rem; width: 3rem;"
-                    el.className = "mx-2"
-                    el.src = iconized_tags[tag]
-                    this.container.appendChild(el)
+                    this.container.append(constructIcon(iconized_tags[tag]))
                   }
 
                 }
@@ -62,3 +80,37 @@ export default class TagIcons extends BaseComponent{
 }
 
 customElements.define("tag-icons",TagIcons)
+
+let constructItem= (item)=>{
+  let container = document.createElement("div")
+  container.className = "d-flex col-6 p-2 align-items-center border"
+  container.append(constructIcon(item))
+  let text = document.createElement("div")
+  text.className = "ml-3"
+  text.innerText = item.desc
+  container.append(text)
+
+  return container
+}
+
+
+export let legend = class TagLegend extends BaseComponent{
+  constructor() {
+    super();
+
+    this.addStyleSheet("/dist/bootstrap-4.3.1-dist/css/bootstrap.min.css")
+
+    let container = document.createElement("div")
+    container.id = "container"
+    container.className = "container d-flex flex-wrap align-items-stretch"
+    this._root.append(container)
+    this.container = container
+
+    for (let icon of Object.values(iconized_tags)){
+      container.append(constructItem(icon))
+    }
+  }
+
+}
+
+customElements.define("tag-legend",legend)
