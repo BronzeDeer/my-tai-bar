88565385a777a4d6f3b89f0f8f8869ea59b79e27
Joel Pepper <pepper@bronze-deer.de>
2022-08-14 01:30:57 +0200

    list: recipe: allow loading price from rich list

--- src/js/component/list/recipe.js
+++ src/js/component/list/recipe.js
@@ -4,12 +4,13 @@ import CocktailList from "/js/component/list/base.js"
 
 export default class RecipeList extends CocktailList{
 
-    makeLoaderFunc = (id) => {
+    makeLoaderFunc = (id,price) => {
         return  async () =>{
             let el = document.createElement("cocktail-recipe")
             el.cocktailID = id
             el.className = "fade-in"
             el.printable = this.printable
+            if (price) el.price = price
             return el
         }
     }
